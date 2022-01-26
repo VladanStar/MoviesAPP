50bf03db2d2f6872b5661d96c281b117b8ddac7d
unknown <vladancupric@gmail.com>
2022-01-26 13:58:33 +0100

    New commits

--- moviesapp/src/App.js
+++ moviesapp/src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Movie from "./components/Movie";
 
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+ const IMG_API = "https://image.tmdb.org/t/p/w1280"; 
+ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 function App() {
-  const movies = ['1','2','3'];
+  // const movies = ['1','2','3'];
+  const[movies, setMovies] = useState([]);
+
+  useEffect( async() => {
+
+    const moviesResp = await fetch(FEATURED_API);
+    const moviesR = await moviesResp.json();
+
+    setMovies(moviesR);
+
+  },[]);
+
 
   return (
     <div className="App">
