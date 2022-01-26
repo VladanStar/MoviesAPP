faacde616d05b17dab40c51ed2103be02629bf5f
unknown <vladancupric@gmail.com>
2022-01-26 13:48:59 +0100

    New commits

--- moviesapp/src/App.js
+++ moviesapp/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Movie from "./components/Movie";
 
 function App() {
+  const movies = ['1','2','3'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {movies.map((movie) => (
+        <Movie />
+      ))}
     </div>
   );
 }
---
+++ moviesapp/src/components/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Movie = () => {
+  return (
+  
+  <div className='movie'>
+      This is a movie Component
+
+  </div>)
+};
+
+export default Movie;
